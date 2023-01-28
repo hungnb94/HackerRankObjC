1e68a09b892192c4d34fedecd44be4d3b26c9c16
Nguyen Ba Hung <hung@bio-rithm.com>
2023-01-28 15:01:18 +0700

    Initial Commit

---
+++ HackerRankObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HackerRankObjc
+//
+//  Created by Hung Ba on 28/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
